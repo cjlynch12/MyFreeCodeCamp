211ba808a85d7a619bb1af87f5409391ed61b874
cjlynch12 <9901174+cjlynch12@users.noreply.github.com>
2018-01-14 12:53:18 -0500

    Create immutable.js

---
+++ redux/immutable.js
@@ -0,0 +1,33 @@
+//example of a immutable state reducer
+const ADD_TO_DO = 'ADD_TO_DO';
+
+// A list of strings representing tasks to do:
+const todos = [
+  'Go to the store',
+  'Clean the house',
+  'Cook dinner',
+  'Learn to code',
+];
+
+const immutableReducer = (state = todos, action) => {
+  switch(action.type) {
+    case ADD_TO_DO:
+      // don't mutate state here or the tests will fail
+      //make a COPY of the state, mutate the new copy, return the new copy
+      let newArray = state.slice();
+      newArray.push(action.todo);
+      return newArray;
+    default:
+      return state;
+  }
+};
+
+// an example todo argument would be 'Learn React',
+const addToDo = (todo) => {
+  return {
+    type: ADD_TO_DO,
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
