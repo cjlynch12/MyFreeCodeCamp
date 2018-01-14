a50504735831ea53ea62650ae993539eb606e01f
cjlynch12 <9901174+cjlynch12@users.noreply.github.com>
2018-01-14 13:22:40 -0500

    Create immutable-object-assign.js

---
+++ redux/immutable-object-assign.js
@@ -0,0 +1,26 @@
+//immutable state using Object.assign({});
+
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      const newObject = Object.assign({}, state, {status: 'online'});
+      return newObject;
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
